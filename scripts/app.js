2acae7d98b0fe1299eb36a2e409f8fff072b279d
andrey skorohod <dp160291sas@gmail.com>
2017-08-02 21:33:16 +0300

    'initial'

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var myApp = angular.module('myApp', []);
+
+myApp.
+    component('itemComponent', {
+        templateUrl: 'main.html',
+        controller: function ItemsController() {
+            self = this;
+            self.textarea = '';
+            self.items = JSON.parse(localStorage.getItem('items')) || [];
+
+            self.addItem = function (item) {
+                self.items.push({ id: Math.floor((Math.random() * 1000000) + 1), text: item, comments: [] });
+                localStorage.setItem('items', JSON.stringify(self.items))
+                
+            };
+
+            self.delItem = function (id) {
+                self.items = self.items.filter(item => {
+                    return item.id != id;
+                });
+                localStorage.setItem('items', JSON.stringify(self.items))
+
+            };
+            self.selected = null;
+
+            self.select = function (item) {
+                self.selected = item;
+            }
+
+            self.showComments = function (item) {
+                self.comments = item.comments;
+            }
+
+            self.addComment = function (textarea) {
+                self.comments.push({ id: Math.floor((Math.random() * 1000000) + 1), text: textarea });
+                localStorage.setItem('items', JSON.stringify(self.items))
+                self.textarea = '';
+                
+            }
+        }
+    });
+
